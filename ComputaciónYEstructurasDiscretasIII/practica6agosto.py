a77a67e4626d3c801833d2fc978353c9b440b89b
Santiago Valencia <svalenciagarcia707@gmail.com>
2024-12-05 23:17:23 -0500

    Añadiendo casi todo

--- ComputaciónYEstructurasDiscretasIII/practica6agosto.py
+++
@@ -1,4 +0,0 @@
-import re
-# Para escapar un caracter especial, debo usar un doble back slash o meterlo dentro de un row string con una r antes del string
-print("hello \\n world")
-print(r"hello \n world")
